410449649cac6a6ee283f290c3a4dcf10ef080c4
flaska99 <tmdalsehrwhs@naver.com>
2025-06-16 00:33:51 +0900

    add : swagger_ui (api 명세)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  // swagger 설정
+  const config = new DocumentBuilder()
+        .setTitle('정글 커뮤니티 API')
+        .setDescription('정글 커뮤니티 백엔드 API 명세서')
+        .setVersion('1.0')
+        .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
