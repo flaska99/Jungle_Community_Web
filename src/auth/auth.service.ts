af58167f9630d05769663a972c9b0eb9e1e439e0
flaska99 <tmdalsehrwhs@naver.com>
2025-06-16 15:54:33 +0900

    add : auth with jwt (login)

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/Entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository : Repository<User>,
+        private jwtservice : JwtService
+    ) {}
+
+    async validateUser(user_id : string, password : string) : Promise<User>{
+        const user = await this.userRepository.findOne({where : {user_id}});
+        if (!user) throw new UnauthorizedException('존재하지 않는 유저입니다.');
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) throw new UnauthorizedException('비밀번호가 틀렸습니다.');
+
+        return user;
+    }
+
+    async login(user : User){
+        const payload = { sub : user.id, user_id : user.user_id };
+        return {
+            access_token: this.jwtservice.sign(payload),
+        };
+    }
+
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,27 +3,28 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './Entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { QueryFailedError } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(
+    constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-  ) {}
+    ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const user = this.userRepository.create(createUserDto);
-    
+    async create(createUserDto: CreateUserDto): Promise<User> {
+    const { password, ...rest } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.userRepository.create({ ...rest, password: hashedPassword });
+
     try {
-      return await this.userRepository.save(user);
-    } catch (error) {
-      // TypeORM의 에러코드가 1062이면 unique 제약조건 위반 (MySQL 기준)
-      if (error.code === 'ER_DUP_ENTRY' || error.errno === 1062) {
+    return await this.userRepository.save(user);
+    } catch (error: any) {
+    if (error?.code === 'ER_DUP_ENTRY' || error?.errno === 1062) {
         throw new ConflictException('이미 존재하는 아이디 또는 닉네임입니다.');
-      }
-      throw new InternalServerErrorException();
     }
-  }
+    throw new InternalServerErrorException();
+    }
+    }
 }
 
