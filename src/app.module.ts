94a99e02ec39e72ea4b6cd52051fc44774bd1b8d
flaska99 <tmdalsehrwhs@naver.com>
2025-06-16 22:30:21 +0900

    error 해결 1차 시도

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { PostController } from './post/post.controller';
-import { PostModule } from './post/post.module';
-import { CategoryModule } from './category/category.module';
-import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
@@ -24,10 +19,6 @@ import { CommentModule } from './comment/comment.module';
       synchronize: true, // 개발단계만 true
     }),
     UserModule,
-    AuthModule,
-    PostModule,
-    CategoryModule,
-    CommentModule,
   ],
 })
 export class AppModule {}
