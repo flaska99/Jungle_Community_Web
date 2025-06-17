e8dfc482148a5c9c5dd121e50a1416b0731c988d
flaska99 <tmdalsehrwhs@naver.com>
2025-06-17 21:58:56 +0900

    조회수 useguard 사용

--- src/post-view/post-view.controller.ts
+++ src/post-view/post-view.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Param, Patch, Request } from '@nestjs/common';
+import { Controller, Param, Patch, Request, UseGuards } from '@nestjs/common';
 import { PostViewService } from './post-view.service';
+import { JwtAuthGuard } from 'src/auth/strategy/jwt.authGuard';
 
 @Controller('post-view')
 export class PostViewController {
   constructor(private readonly postViewService: PostViewService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':postId/view')
   async increaseView(@Param('postId') postId : string, @Request() req){
     const user_id = req.user.sub;
