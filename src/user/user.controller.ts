518d049edbf97328f18c9f5cef5d9c2d6b5b5afc
flaska99 <tmdalsehrwhs@naver.com>
2025-06-17 16:51:25 +0900

    add : userinfo (유저의 정보 추가)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Request } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './Entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/strategy/jwt.authGuard';
 
 @Controller('users')
 export class UserController {
@@ -10,5 +11,11 @@ export class UserController {
   @Post('signup')
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
+  }
+
+  @Get('userinfo')
+  @UseGuards(JwtAuthGuard)
+  async getUserInfo(@Request() req) : Promise<number> {
+    return this.userService.findMyPost(req.user.sub);
   }
 }
