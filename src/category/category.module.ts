623b46e7cb0da481f3a1c9d92f16ba8069bd2bbf
flaska99 <tmdalsehrwhs@naver.com>
2025-06-17 14:53:51 +0900

    comment 의존성 주입

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from 'src/comment/Entities/comment.entity';
+import { CommentService } from 'src/comment/comment.service';
+import { CommentController } from 'src/comment/comment.controller';
+import { User } from 'src/user/Entities/user.entity';
+import { Post } from 'src/post/Entities/post.entity';
 
-@Module({})
-export class CategoryModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Comment, User, Post])],
+  controllers: [CommentController],
+  providers: [CommentService],
+})
+export class CommentModule {}
