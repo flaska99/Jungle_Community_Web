f5412fcb6f421c1b13e40d2f55595ddae92bbb0b
flaska99 <tmdalsehrwhs@naver.com>
2025-06-17 21:52:40 +0900

    post-view url 수정

--- src/post-view/post-view.controller.ts
+++ src/post-view/post-view.controller.ts
@@ -5,7 +5,7 @@ import { PostViewService } from './post-view.service';
 export class PostViewController {
   constructor(private readonly postViewService: PostViewService) {}
 
-  @Patch('postId/view')
+  @Patch(':postId/view')
   async increaseView(@Param('postId') postId : string, @Req() req){
     const user_id = req.user.sub;
     return this.postViewService.increaseView(user_id, postId);
