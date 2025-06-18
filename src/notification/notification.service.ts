90208452dd3559b9a607c0844f32d45e8494aa77
flaska99 <tmdalsehrwhs@naver.com>
2025-06-18 22:33:58 +0900

    socket notify postid 인자 추가!

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { NotificationGateway } from './notification.gateway';
+import { Post } from 'src/post/Entities/post.entity';
 
 @Injectable()
 export class NotificationService {
     constructor(private readonly notificationGateway : NotificationGateway){}
 
-    notify(userId: string, postId : string, message: string){
-        this.notificationGateway.sendNotification(userId, postId, message);
+    notify(userId: string, post : Post, message: string){
+        this.notificationGateway.sendNotification(userId, post, message);
     }
 }
